102148cfba337b22f4d93932643df5652a3a7ff3
Michael Peterson <michaelpp2001@gmail.com>
2019-09-25 21:33:49 -0700

    added a red wbg list view, and added more wines to the backend

--- Frontend/src/app/red-wine/red-wine.component.ts
+++ Frontend/src/app/red-wine/red-wine.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WineService } from '../wine.service';
 
 @Component({
   selector: 'app-red-wine',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RedWineComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _wine: WineService) { }
 
   ngOnInit() {
   }
