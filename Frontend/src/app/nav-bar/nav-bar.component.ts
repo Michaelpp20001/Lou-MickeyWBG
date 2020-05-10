e356c6175002fb3ff28768412d52874d7848fbd6
Michael <michaelpp2001@gmail.com>
2020-05-09 21:41:49 -0700

    added tabs to be highlighted when on the correct page

--- Frontend/src/app/nav-bar/nav-bar.component.ts
+++ Frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _search: SearchService) { }
+
+  tab = 1;
 
   ngOnInit() {
+    console.log(this.tab);
+  }
+
+  toggleClass() {
+    console.log(this.tab);
   }
 
 }
